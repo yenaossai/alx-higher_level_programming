f80b79c0316b5c4fa44b35609423a8fc32ce8a56
yenaossai <yenaossai@gmail.com>
2023-11-28 18:42:38 +0000

    python if else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_num = number % -10
+    last_num = ((number * -1) % 10) * -1
 elif number >= 0:
     last_num = number % 10
     if last_num > 5:
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n < 0:
