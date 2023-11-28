6837aaa8c1371f4b08bde5d77e73ecfa4d72f4b8
yenaossai <yenaossai@gmail.com>
2023-11-28 00:24:22 +0000

    python hello world update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -20,5 +20,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LISTS_H */
