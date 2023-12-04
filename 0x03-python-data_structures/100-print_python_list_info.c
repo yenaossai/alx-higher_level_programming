e18e40a11e71919325ab3be7460d46511761d666
yenaossai <yenaossai@gmail.com>
2023-12-04 19:01:48 +0000

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,16 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <Python.h>
+
 /**
- * print_python_list_info - prints some basic info about Python lists.
- * * @p: python list
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
  */
 void print_python_list_info(PyObject *p)
 {
-	int elem;
+    int size, i;
+    PyObject *obj;
 
-	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (elem = 0; elem < Py_SIZE(p); elem++)
-		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+    if (!PyList_Check(p))
+    {
+        fprintf(stderr, "Invalid argument. Not a Python list.\n");
+        return;
+    }
+
+    size = Py_SIZE(p);
+
+    printf("[*] Size of the Python List = %d\n", size);
+
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        obj = PyList_GetItem(p, i);
+
+        if (obj == NULL)
+        {
+            fprintf(stderr, "Error getting item at index %d.\n", i);
+            continue;
+        }
+
+        printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
+    }
 }
