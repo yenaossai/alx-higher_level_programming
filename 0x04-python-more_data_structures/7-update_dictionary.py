d269faea077a58b744600708011e2470088c0f6f
yenaossai <yenaossai@gmail.com>
2023-12-06 22:05:33 +0000

    updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictonary, key, value):
-    my_dictionary[key] = value
-    return my_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictonary, key=""):
-    if key in my_dictionary:
-        del my_dictionary[key]
-    return my_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
