cda30d25cbdadbb20babf6c800c67af4bee13501
yenaossai <yenaossai@gmail.com>
2023-12-06 22:02:24 +0000

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,25 +3,6 @@
 #include <listobject.h>
 #include <bytesobject.h>
 
-void print_python_list(PyObject *p)
-{
-        long int size = PyList_Size(p);
-        int i;
-        PyListObject *list = (PyListObject *)p;
-        const char *type;
-
-        printf("[*] Python list info\n");
-        printf("[*] Size of the Python List = %li\n", size);
-        printf("[*] Allocated = %li\n", list->allocated);
-        for (i = 0; i < size; i++)
-        {
-                type = (list->ob_item[i])->ob_type->tp_name;
-		printf("Element %i: %s\n", i, type);
-                if (!strcmp(type, "bytes"))
-                        print_python_bytes(list->ob_item[i]);
-        }
-}
-
 void print_python_bytes(PyObject *p)
 {
 	long int size;
@@ -48,3 +29,21 @@ void print_python_bytes(PyObject *p)
 	printf("\n");
 }
 
+void print_python_list(PyObject *p)
+{
+        long int size = PyList_Size(p);
+        int i;
+        PyListObject *list = (PyListObject *)p;
+        const char *type;
+
+        printf("[*] Python list info\n");
+        printf("[*] Size of the Python List = %li\n", size);
+        printf("[*] Allocated = %li\n", list->allocated);
+        for (i = 0; i < size; i++)
+        {
+                type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+                if (!strcmp(type, "bytes"))
+                        print_python_bytes(list->ob_item[i]);
+        }
+}
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
+def simple_delete(a_dictonary, key=""):
     if key in my_dictionary:
         del my_dictionary[key]
     return my_dictionary
